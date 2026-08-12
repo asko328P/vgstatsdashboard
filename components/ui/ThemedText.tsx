4cbd2c0c0c3392d1e6e0c1dc9cf0358ffe7195f3
Asad Basic <asad.basic@misija.io>
2026-08-12 21:16:37 +0200

    unistyles config

--- components/ui/ThemedText.tsx
+++ components/ui/ThemedText.tsx
@@ -1,11 +1,21 @@
-import { Text, type TextProps, StyleSheet } from "react-native";
+import { Text, type TextProps } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
 
-import { useThemeColor } from "@/hooks/useThemeColor";
+// import { useThemeColor } from '@/hooks/useThemeColor';
+
+export type TextTypes =
+  | "default"
+  | "title"
+  | "defaultSemiBold"
+  | "subtitle"
+  | "link"
+  | "defaultSemiBoldMedium"
+  | "small";
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link";
+  type?: TextTypes;
 };
 
 export function ThemedText({
@@ -15,17 +25,21 @@ export function ThemedText({
   type = "default",
   ...rest
 }: ThemedTextProps) {
-  const color = "white";
+  // const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
     <Text
       style={[
-        { color },
+        styles.text,
         type === "default" ? styles.default : undefined,
         type === "title" ? styles.title : undefined,
         type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
+        type === "defaultSemiBoldMedium"
+          ? styles.defaultSemiBoldMedium
+          : undefined,
         type === "subtitle" ? styles.subtitle : undefined,
         type === "link" ? styles.link : undefined,
+        type === "small" ? styles.small : undefined,
         style,
       ]}
       {...rest}
@@ -33,7 +47,10 @@ export function ThemedText({
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create((theme) => ({
+  text: {
+    color: theme.colors.textPrimary,
+  },
   default: {
     fontSize: 16,
     lineHeight: 24,
@@ -43,18 +60,29 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     fontWeight: "600",
   },
+  defaultSemiBoldMedium: {
+    fontSize: 20,
+    lineHeight: 24,
+    fontWeight: "700",
+  },
   title: {
     fontSize: 32,
     fontWeight: "bold",
     lineHeight: 32,
   },
   subtitle: {
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: "bold",
   },
   link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: "#0a7ea4",
+    lineHeight: 24,
+    // fontSize: 14,
+    color: theme.colors.primary,
+    textDecorationLine: "underline",
+    textDecorationColor: theme.colors.primary,
   },
-});
+  small: {
+    fontSize: 12,
+    lineHeight: 18,
+  },
+}));
